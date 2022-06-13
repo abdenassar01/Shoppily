cca4af97c3a0dc0dfaca6d83aa4ec79a3be3ec31
abdenassar01 <abdenassaramimi99@gmail.com>
2022-06-13 12:02:09 +0100

    prepare to deploy to aws elastic beanstalk

---
+++ E-commerce-api/src/main/java/com/api/ecommerce/controller/Public.java
@@ -0,0 +1,17 @@
+package com.api.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.PermitAll;
+
+@RestController
+@PermitAll
+public class Public {
+    
+    @GetMapping("/")
+    public String sayHey(){
+        return "Hello and Welcome to Shoppily api";
+    }
+}
