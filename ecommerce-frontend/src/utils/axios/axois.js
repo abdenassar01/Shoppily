dcafc9aa02f3edbb15d303c361f4e34ad74e969f
abdenassar01 <abdenassaramimi99@gmail.com>
2022-08-02 00:34:58 +0100

    generating new project for learning

--- ecommerce-frontend/src/utils/axios/axois.js
+++ ecommerce-frontend/src/utils/axios/axois.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const main = axios.create({
-    baseURL: "http://springbootecommerceapi-env.eba-n8zcs5z8.eu-west-3.elasticbeanstalk.com"
-    //baseURL: "http://localhost:5000"
+    //baseURL: "http://springbootecommerceapi-env.eba-n8zcs5z8.eu-west-3.elasticbeanstalk.com"
+    baseURL: "http://localhost:5000"
 });
 
 export const extended = axios.create({
-    baseURL: "http://springbootecommerceapi-env.eba-n8zcs5z8.eu-west-3.elasticbeanstalk.com/api/v1"
-    //baseURL: "http://localhost:5000/api/v1"
+    //baseURL: "http://springbootecommerceapi-env.eba-n8zcs5z8.eu-west-3.elasticbeanstalk.com/api/v1"
+    baseURL: "http://localhost:5000/api/v1"
 })
